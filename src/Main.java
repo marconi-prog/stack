5a82e82d04e35a9986a28c76d7fe7e39d8777a17
marco <marconifarias82@gmail.com>
2025-10-24 20:54:46 -0300

    uso do linkedlist

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -9,21 +10,40 @@ public class Main {
         ninjasArray[0] = "Naruto";
         ninjasArray[1] = "Sasuke";
         ninjasArray[2] = "Sakura";
+
+
         //List
         List<String> ninjasList = new ArrayList<>();
         ninjasList.add("naruto");
         ninjasList.add("sasuke");
         ninjasList.add("sakura");
+
+
         //stack
         Stack<String> ninjasStack = new Stack<>();
         ninjasStack.add("naruto");
+
         ninjasStack.push("sasuke");
+
         ninjasStack.push("sakura");
+
         System.out.println("minha stack " + ninjasStack);
+
         ninjasStack.pop();
+
         System.out.println("minha stack depois do pop " + ninjasStack);
         System.out.println("minha stack peek " + ninjasStack.peek());
         System.out.println("tamanho da stack " + ninjasStack.size());
+
+
+        //LinkedList
+        LinkedList<String> ninjasLinkedList = new LinkedList<>();
+        ninjasLinkedList.add("naruto");
+        ninjasLinkedList.add(0,"sasuke");
+
+        System.out.println("minha linkedlist " + ninjasLinkedList);
+        System.out.println("minha linkedlist " + ninjasLinkedList);
+        
 
     }
 }
